c53a070ecb660437fee6fc925ee0208a790b6418
vladimir-simek <minervladik@gmail.com>
2021-03-14 10:24:02 +0100

    1/4

--- src/com/vladimirsimek/linearfunctions/Data.java
+++ src/com/vladimirsimek/linearfunctions/Data.java
@@ -1,5 +1,42 @@
 package com.vladimirsimek.linearfunctions;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Data {
 
+    public static ArrayList<String[]> rawFunctions = new ArrayList<>();
+
+    public static void loadToArrayList(){
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("data/functions.txt"));
+            String readLine = bufferedReader.readLine();
+
+            while (readLine != null) {
+
+                String[] validLinePrepare = readLine.split(" ");
+                String[] validLine = new String[2];
+
+                validLine[0] = validLinePrepare[2];
+                validLine[1] = validLinePrepare[4];
+
+                rawFunctions.add(validLine);
+
+                readLine = bufferedReader.readLine();
+            }
+
+            /*for (String[] rawFunction : rawFunctions) {
+                System.out.println(Arrays.toString(rawFunction));
+            }*/
+
+            // for test uncomment code above
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/com/vladimirsimek/linearfunctions/Logic.java
+++ src/com/vladimirsimek/linearfunctions/Logic.java
@@ -1,7 +1,9 @@
 package com.vladimirsimek.linearfunctions;
 
+import static com.vladimirsimek.linearfunctions.Data.loadToArrayList;
+
 public class Logic {
     public static void init() {
-
+        loadToArrayList();
     }
 }
